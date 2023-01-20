9b50c959b65be099d3f0fcc9ac0070038c3e3cd7
Cash2311 <86266624+Cash2311@users.noreply.github.com>
2023-01-19 19:33:36 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 img = "";
 stat = "";
+objects = [];
+
 function preload() {
     img = loadImage('dog_cat.jpg');
 }
@@ -21,9 +23,24 @@ function gotResult(error, results) {
         console.log(error);
     }
     console.log(results);
+    objects = results;
 }
 function draw() {
     image(img, 0, 0, 640, 420);
+    if(stat != "")
+    {
+        for (i = 0; i < objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
     fill("#FF0000");
     text("Dog", 45, 75);
     noFill();
